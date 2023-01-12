a5ec1af5edfc423ed2a94f7d32c551840ab27bc6
Prashant Shahaji Hatekar <108356540+whitehat65@users.noreply.github.com>
2023-01-12 20:59:18 +0530

    Update Check_number_is_odd_or_even.js

--- Check_number_is_odd_or_even.js
+++ Check_number_is_odd_or_even.js
@@ -1,1 +1,9 @@
-
+<div id="display"></div>
+   <script>
+      let numoutput=document.getElementById("display");
+      var nmbr = 254;
+      if(nmbr % 2 == 0)
+         numoutput.innerHTML = nmbr + " is Even";
+      else
+         numoutput.innerHTML = nmbr + " is Odd";
+   </script>
